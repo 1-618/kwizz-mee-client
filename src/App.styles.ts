eb5bb8dcb86caec6d8c736a036f7dc9daa7e1d6e
Aryscene <aryscene@gmail.com>
2021-09-15 20:30:06 +0200

    minor css changes

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -7,7 +7,8 @@ html {
 }
 
 body {
-  background-image: url(${BGImage});
+  background: -webkit-linear-gradient(to right, #fc00ff, #00dbde); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #fc00ff, #00dbde);
   background-size: cover;
   margin: 0;
   padding: 0 20px;
@@ -49,71 +50,71 @@ display: flex;
   .start {
     width: 250px;
     margin: 10px;
-    border: 2px solid #ffa500;
+    border: 2px solid #0085a3;
     padding: 6px 24px;
     font-size: 1.15em;
     letter-spacing: 1px;
     text-transform: uppercase;
-    color: #ffa500;
+    color: #0085a3;
     background-color: #f1f2f2;
     transition: all 0.25s ease-in-out;
   }
   .next {
     width: 250px;
     margin: 10px;
-    border: 2px solid #ffa500;
+    border: 2px solid #0085a3;
     padding: 6px 24px;
     font-size: 1.15em;
     letter-spacing: 1px;
     text-transform: uppercase;
-    color: #ffa500;
+    color: #0085a3;
     background-color: #f1f2f2;
     transition: all 0.25s ease-in-out;
   }
   .start:hover {
-    background-color: #ffbc01;
-    border-color: #ffbc01;
+    background-color: #0085a3;
+    border-color: #0085a3;
     color: #f1f2f2;
   }
 
   .next:hover {
-    background-color: #ffbc01;
-    border-color: #ffbc01;
+    background-color: #de0797;
+    border-color: #de0797;
     color: #f1f2f2;
   }
   .start:active {
-    background-color: #ffbc01;
-    border-color: #ffbc01;
+    background-color: #de0797;
+    border-color: #de0797;
     color: #f1f2f2;
     box-shadow: none;
   }
 
   .next:active {
-    background-color: #ffbc01;
-    border-color: #ffbc01;
+    background-color: #de0797;
+    border-color: #de0797;
     color: #f1f2f2;
     box-shadow: none;
   }
   .start:focus, .start:active:focus {
-    background-color: #ffbc01;
-    border-color: #ffbc01;
+    background-color: #de0797;
+    border-color: #de0797;
     color: #f1f2f2;
-    outline: 5px auto #ffbc01;
+    outline: 5px auto #de0797;
   }
   
   .next:focus, .next:active:focus {
-    background-color: #ffbc01;
-    border-color: #ffbc01;
+    background-color: #de0797;
+    border-color: #de0797;
     color: #f1f2f2;
-    outline: 5px auto #ffbc01;
+    outline: 5px auto #de0797;
   }
   .start:visited {
-    background-color: #ffbc01;
-    border-color: #ffbc01;
+    background-color: #de0797;
+    border-color: #de0797;
   }
 
   .next:visited {
-    background-color: #ffbc01;
-    border-color: #ffbc01;
+    background-color: #de0797;
+    border-color: #de0797;
   }
   `
